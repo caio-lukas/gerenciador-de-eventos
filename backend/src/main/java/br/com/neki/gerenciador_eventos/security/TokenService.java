ec0ec531c4fdb854f3511a0f9219fdc8b3e36aad
caio-lukas <caiolukasmm@outlook.com.br>
2026-08-12 02:23:45 -0300

    feat: implementa geracao e validacao de token jwt

---
+++ backend/src/main/java/br/com/neki/gerenciador_eventos/security/TokenService.java
@@ -0,0 +1,46 @@
+package br.com.neki.gerenciador_eventos.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class TokenService {
+    public static final int UM_DIA_EM_MILISSEGUNDOS = 1000 * 60 * 60 * 24;
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    // todo: Implementar entidade Admin
+    public String generateToken(Admin admin) {
+        return Jwts.builder()
+                .setSubject(admin.getEmail())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + UM_DIA_EM_MILISSEGUNDOS))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String validateToken(String token) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
