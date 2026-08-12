2c130fd9c1e41b2ec25ce07edb2a6c4f94c89ab3
caio-lukas <caiolukasmm@outlook.com.br>
2026-08-12 02:48:37 -0300

    feat: configura filtro de segurança jwt e criptografia bcrypt

---
+++ backend/src/main/java/br/com/neki/gerenciador_eventos/security/SecurityFilter.java
@@ -0,0 +1,53 @@
+package br.com.neki.gerenciador_eventos.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    // todo: Implementar repositório do admin
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+
+        if (token != null) {
+            var email = tokenService.validateToken(token);
+
+            if (email != null) {
+                // todo: Implementar entidade Admin
+                Admin admin = adminRepository.findByEmail(email).orElse(null);
+
+                if (admin != null) {
+                    var authentication = new UsernamePasswordAuthenticationToken(admin, null, Collections.emptyList());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return null;
+        }
+        return authHeader.replace("Bearer ", "");
+    }
+}
